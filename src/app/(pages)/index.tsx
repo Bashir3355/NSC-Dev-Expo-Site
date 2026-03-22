59072f0cfdef61112ea1c954505735eba3b8be38
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-03-22 09:54:00 -0700

    Add ScrollView to main pages

--- src/app/(pages)/index.tsx
+++ src/app/(pages)/index.tsx
@@ -1,13 +1,23 @@
-import { View, Text } from 'react-native';
+import { ScrollView, Text, StyleSheet } from "react-native";
 import Hello from '../../components/Hello';
 import Hero from "../../components/Hero";
+import ContactUsBanner from "src/components/ContactUsBanner";
 
 export default function HomePage() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <ScrollView contentContainerStyle={styles.container}>
       <Hero />
       <Text>Welcome to the Home Page!</Text>
       <Hello/>
-    </View>
+      <ContactUsBanner />
+    </ScrollView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
